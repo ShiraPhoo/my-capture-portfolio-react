406f43f49e8abb78f91444ce9ee05756965e113a
Hyper Phoo <hyperwebdev.codecentury@gmail.com>
2020-12-17 21:23:56 +0630

    About Page Completed

---
+++ src/components/FaqSection.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+function FaqSection() {
+  return (
+    <div>
+      <div className="faq">
+        <h2>
+          Any questions <span>FAQ</span>
+        </h2>
+
+        <div className="question">
+          <h4>How Do I Start?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              odit.
+            </p>
+          </div>
+        </div>
+
+        <div className="question">
+          <h4>What products do you offer?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              odit.
+            </p>
+          </div>
+        </div>
+
+        <div className="question">
+          <h4>Different Payment Methods?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              odit.
+            </p>
+          </div>
+        </div>
+
+        <div className="question">
+          <h4>Daily Schedule?</h4>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              s Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+              odit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
